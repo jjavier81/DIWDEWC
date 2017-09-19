952b56296ecd27f588b8699d57d298b38354a75f
Jose Javier Perez Lorente <javiperez81@hotmail.com>
2017-09-19 22:18:30 +0200

    Vamos para arriba

---
+++ DWEC/PRUEBAS/js/mensaje.js
@@ -0,0 +1,8 @@
+alert(t);
+alert("Esto es una cadena por alert");
+console.log("Esto es una cadena por console.log");
+console.info("Esto es un info");
+console.warn("Esto es un aviso");
+console.error("Esto es un error");
+document.write("<h1>Esto es document write</h1>");
+var t = prompt("Introduce tu nombre");
