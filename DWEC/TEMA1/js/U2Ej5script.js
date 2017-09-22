0e553317d391e02b7420f4dd9aaf1e2a6ce97d5f
Jose Javier Perez Lorente <javiperez81@hotmail.com>
2017-09-22 18:17:43 +0200

    Ejercicio 5

--- DWEC/TEMA1/js/U2Ej5script.js
+++ DWEC/TEMA1/js/U2Ej5script.js
@@ -1,2 +1,4 @@
-var rima =  prompt("Dime una rima");
-console.log("rima");
+var rima1 =  prompt("Dime una rima de dos lineas.\tPrimera:");
+var rima2 = prompt("Segunda y final:");
+console.log("rima1");
+console.log("rima2");
